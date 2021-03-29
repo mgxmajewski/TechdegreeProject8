67161fc8bd29992a6be3af9a0dde64ecf21f40a5
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-30 00:56:45 +0200

    Log out JSON with all books from db on home route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+    (async () => {
+        const books = await Book.findAll()
+        res.json(books)
+        console.log(books)
+    })()
 });
 
 module.exports = router;
